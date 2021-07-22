ac13d5f2a95c061c5daf3a16d92a96248dd0b611
Ivan Daum <ivan.daum@gmail.com>
2021-07-22 20:27:35 +0200

    feat(scripts): add function for random float

--- scripts/math.js
+++ scripts/math.js
@@ -5,3 +5,7 @@ export const range = (input, min, max) => ((input - min) * 100) / (max - min);
 export const lerp = (v0, v1, t) => (1 - t) * v0 + t * v1;
 
 export const createUniqueIndex = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
+
+export const distance2d = (p1, p2) => Math.sqrt((Math.pow) + (Math.pow(p2[1] - p1[1], 2)));
+
+export const roundFloat = (d) => Math.floor(d * 100) * 0.01;

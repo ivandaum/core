4b5d4b8449c9cd8c6e9e6844e4b9da137d3a0f33
Ivan Daum <ivan.daum@gmail.com>
2021-11-12 09:45:47 +0100

    feat(root): add raf-manager plugin for vue

---
+++ vue-plugins/raf-manager.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+
+const createUniqueIndex = () => Date.now().toString(36) + Math.random().toString(36).substr(2);
+
+class RafManager {
+  constructor() {
+    this.callbacks = [];
+    this.render();
+  }
+
+  addQueue(func) {
+    const index = createUniqueIndex();
+    this.callbacks[index] = func;
+    return index;
+  }
+
+  removeQueue(index) {
+    if (this.callbacks[index]) {
+      delete this.callbacks[index];
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    this.raf = requestAnimationFrame(this.render.bind(this));
+    Object.keys(this.callbacks).forEach((key) => {
+      if (this.callbacks[key]) {
+        this.callbacks[key]();
+      }
+    });
+  }
+};
+
+
+Vue.prototype.RafManager = new RafManager();
+
